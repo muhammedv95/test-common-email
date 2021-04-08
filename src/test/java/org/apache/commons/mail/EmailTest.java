1b061ed3ac1d923e3a273f42a5eb924f4305ea91
Muhammed <muhammed@MacBook-Pro.local>
2021-04-08 18:40:03 -0400

    Create and added datasets with test emails and instantiated Email class, along with set up method (before) and teardown method (after)

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,5 +1,52 @@
 package org.apache.commons.mail;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Date;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class EmailTest {
 
+	private static final String[] Test_Emails = { "ab@c.com", "a.b@c.org", "abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd" };
+	private static final String[] Empty_Emails = null;
+	
+	// Concrete Email Class for Testing
+	private EmailConcrete email;
+	
+	@Before		// set up method
+	public void setUpEmailTest() throws Exception {
+		
+		email = new EmailConcrete();
+		
+		//mockSmtpServer = new MockSmtpServer(EmailConfiguration.MOCK_SMTP_PORT);
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception {
+		
+		// don't need to write anything, just blank
+		// can write something if want to
+		
+	}
+	
+	
+	
+	
+	
+	
 }
