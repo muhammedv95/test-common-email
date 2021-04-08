75223e56ea23388d9a6abf00fee81e934f8c19b0
Muhammed <muhammed@MacBook-Pro.local>
2021-04-08 18:54:56 -0400

    add test cases for addCc method for up to 70% or more coverage

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -43,10 +43,46 @@ public class EmailTest {
 		// can write something if want to
 		
 	}
+	// addBcc Test Cases
+	@Test
+	public void testAddBcc() throws Exception {
+
+		email.addBcc(Test_Emails);
+		
+		assertEquals(3,  email.getBccAddresses().size());
+
+		// assertEquals -- used to make sure expected & current behavior is equal
+		
+	}
 	
+	@Test(expected = EmailException.class)
+	public void testAddBccEmpty() throws Exception {
+		
+		email.addBcc(Empty_Emails);
+		
+		assertEquals(0, email.getBccAddresses().size());
+	}
+
 	
-	
-	
+	// addCC Test Cases
+	@Test
+	public void testAddCc() throws Exception {
+		// email.addCc(Test_Emails);
+		// assertEquals(3, this.email.getCcAddresses().size());
+		
+		List<InternetAddress> addCcTesting = new ArrayList<InternetAddress>();
+		addCcTesting.add(new InternetAddress("abc@def.com"));
+		addCcTesting.add(new InternetAddress("ghi@jkl.com"));
+		addCcTesting.add(new InternetAddress("mno@pqr.com"));
+		
+		for(int i = 0; i < Test_Emails.length; i++)
+		{
+			this.email.addCc(Test_Emails[i]);
+		}
+
+		assertEquals(addCcTesting.size(), this.email.getCcAddresses().size());
+	} 
+
 	
 	
 }
